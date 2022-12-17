8fdc07cce0690807667194d1f73f9e06b8e21156
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2022-12-17 12:47:24 +0100

    Delete ejercicio1 directory

--- ejercicio1/js/main.js
+++
@@ -1,7 +0,0 @@
-import {DNI} from "./dni.js";
-
-let inputData = document.getElementById("dni").value;
-let checkBtn = document.getElementById("checkBtn");
-let check = new DNI();
-
-checkBtn.onclick = check.letterChecker(inputData);
