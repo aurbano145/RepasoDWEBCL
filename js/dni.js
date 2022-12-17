750e4f741701e8db8cfb516de091f0f28b1eabc6
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2022-12-17 14:23:44 +0100

    Add files via upload

--- js/dni.js
+++ js/dni.js
@@ -1,29 +1,42 @@
 class DNI {
-    letterChecker(data) {
-        let message = document.getElementById("message");
-        if(0 > data.length > 9) {
-            message.innerHTML = "The format introduced is wrong";
-            console.log("Wrong format");
-        } else if(data.length = 9) {
-            console.log(data);
-            var checkArray = JSON.stringify(data).split('');
+    letterChecker() {
+        var message = document.getElementById("message");
+        var data = document.getElementById("dni").value;
+        var letters = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-            for(i = 0; i < 8; i++) {
-                if(typeof checkArray[i] != 'number') {
-                    checkArray.splice(i, 1);
+        if(data.length == 9) {
+            var checkArray = JSON.stringify(data).split("");
+            var numberArray = [];
+
+            for(let i = 0; i < 9; i++) {
+                if(!isNaN(checkArray[i])) {
+                    numberArray.push(checkArray[i]);
                 }
             }
 
-            if(checkArray.length < 8) {
-                message.innerHTML = "The format introduced is wrong";
+            console.log(numberArray);
+
+            if(numberArray.length < 8) {
+                message.innerHTML = "The format entered is wrong";
             } else {
-                var number = checkArray.join('');
+                var number = numberArray.join('');
 
                 number = parseInt(number);
 
                 var remainder = number % 23;
-                console.log(remainder);
+                
+                var newLetter = letters[remainder];
+
+                var result = number + newLetter;
+
+                if(data == result) {
+                    message.innerHTML = "The DNI entered and his letter match.";
+                } else {
+                    message.innerHTML = "The DNI entered and his letter don't match. The correct DNI is " + result + ".";
+                }
             }
+        } else {
+            message.innerHTML = "The format entered is wrong";
         }
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import {DNI} from "./dni.js";
 
-let inputData = document.getElementById("dni").value;
 let checkBtn = document.getElementById("checkBtn");
 let check = new DNI();
 
-checkBtn.addEventListener("click", function() {check.letterChecker(inputData)});
+checkBtn.addEventListener("click", function() {check.letterChecker()});
