67a65e3b3a7f6bb6e3be80d07e87206f2307ae22
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2022-12-17 13:04:37 +0100

    Add files via upload

--- js/editor.js
+++ js/editor.js
@@ -4,33 +4,30 @@ class Editor {
         var editBtn2 = document.getElementById("edit2");
         var text = line.innerHTML;
 
-        var displayEditor = "<textarea id='textEditor' style='resize: none;'>" + text + "</textarea><button id='confirm' class='confirmBtn'>Confirm</button><button id='cancel' class='cancelBtn'>Cancel</button>";
-        var textEditor = document.getElementById("texEditor");
-        var confirmBtn = document.getElementById("confirm");
-        var cancelBtn = document.getElementById("cancel");
-
-        line.innerHTML = displayEditor;
         editBtn1.style.display = "none";
         editBtn2.style.display = "none";
 
-        function confirmEdition() {
-            line.innerHTML = textEditor.innerHTML;
-            editBtn1.style.display = "block";
-            editBtn2.style.display = "block";
-        }
+        var displayEditor = "<textarea id='textEditor' style='resize: none;'>" + text + "</textarea><button id='confirm' class='confirmBtn'>Confirm</button><button id='cancel' class='cancelBtn'>Cancel</button>";
 
-        function cancelEdition() {
-            line.innerHTML = text;
-            editBtn1.style.display = "block";
-            editBtn2.style.display = "block";
-        }
+        line.innerHTML = displayEditor;
+        var confirmBtn = document.getElementById("confirm");
+        var cancelBtn = document.getElementById("cancel");
+        var textEditor = document.getElementById("textEditor");
 
         if(confirmBtn) {
-            confirmBtn.addEventListener("click", () => {confirmEdition()});
+            confirmBtn.addEventListener("click", () => {
+                line.innerHTML = textEditor.value;
+                editBtn1.style.display = "block";
+                editBtn2.style.display = "block";
+            });
         }
 
         if(cancelBtn) {
-            cancelBtn.addEventListener("click", () => {cancelEdition()});
+            cancelBtn.addEventListener("click", () => {
+                line.innerHTML = text;
+                editBtn1.style.display = "block";
+                editBtn2.style.display = "block";
+            });
         }
     }
 }
